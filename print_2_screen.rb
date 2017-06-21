a166a4f888af773783d925a5617acd7880456d02
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-06-21 16:44:25 -0400

    divisible by 3 is mined, all numbers divisible by 5 are minds

--- print_2_screen.rb
+++ print_2_screen.rb
@@ -1,8 +1,18 @@
+i = 1
+
+
 1.upto(100) do |i|
 	if i % 3==0   
 		p "mined"
-	else
-		p i 
-	end
+	elsif i % 5==0
+		p "minds"
+	elsif i % 3!=0
+		p i
+	end	
+		# if i==5
+		# p "minds"
+		# else
+		# p i 
+		# end
 
 end	
