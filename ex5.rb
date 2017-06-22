5feb99185c6e7be0b48613405b0abf6204939876
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-06-22 13:57:48 -0400

    personal facts

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Zeke Legg'
+my_age = 20
+my_height = 72 #inches
+my_weight = 200 #lbs
+my_eyes = 'green'
+my_teeth = 'white'
+my_hair = 'brown'
+
+p "lets talk about #{my_name}."
+p "he's #{my_height} inches tall."
+p " he's #{my_weight} pounds heavy."
+p "actually that's not too heavy."
+p "he's got #{my_eyes} eyes and #{my_hair} hair."
+p " his teeth are usually #{my_teeth} depending on the coffee."
+
+
+p "if I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
