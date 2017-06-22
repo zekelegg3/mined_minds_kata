6cfb20aa4851cdaba983b00b76bf21f5bae1d8cc
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-06-22 14:40:10 -0400

    terrible joke alert

---
+++ ex6.rb
@@ -0,0 +1,21 @@
+types_of_people = 10
+x = "there are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "those who know #{binary} and those who #{do_not}."
+
+p x 
+p y 
+
+p "I said: #{x}."
+p "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "isn't that joke so funny?! #{hilarious}"
+
+p joke_evaluation
+
+w = "this is the left side of..."
+e = "a string with a right side"
+
+p w + e 
