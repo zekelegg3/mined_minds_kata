da1c5b817887c0e37308a74708c50f5479477059
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-06-22 11:50:43 -0400

    exercise 3

---
+++ ex3.rb
@@ -0,0 +1,21 @@
+p "hens #{25 +30 / 6}"
+p "roosters #{100 - 25 * 3 % 4}"
+
+p "now I will count the eggs:"
+
+p 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+
+p "is it true that 3 + 2 < 5 - 7?"
+
+p 3 + 2 < 5 - 7 
+
+p "what is 3 + 2? #{3 + 2}"
+p "what os 5 - 7? #{5 - 7}"
+
+p "oh that's why it's false."
+
+p "how about some more."
+
+p "is it greater? #{5 > -2}"
+p "is it greater or equal? #{5 >= -2}"
+p "is it less or equal? #{5 >= -2}"
