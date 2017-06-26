bbe655bc4afdaa41a9da4045b2671f69050f463a
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-06-26 10:21:26 -0400

    final modified homework, all numbers div by 5 and 3 are mined minds

--- print_2_screen.rb
+++ print_2_screen.rb
@@ -2,11 +2,13 @@ i = 1
 
 
 1.upto(100) do |i| #is the loop to 100
-	if i % 3==0    #if a number is divisible by 3 put mined
+	if i % 15 == 0 
+		p "mined minds"
+	elsif i % 3==0    #if a number is divisible by 3 put mined
 		p "mined"
 	elsif i % 5==0  #if a number is divisible by 5 put minds
 		p "minds"   
-	elsif i % 3!=0  #if a number is NOT divisible by 3 then put the number
+	else  #if a number is NOT divisible by 3 then put the number
 		p i
 	end	
 		# if i==5
