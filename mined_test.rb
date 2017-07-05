76be043218c66be38146aeb2da37831e30100c27
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-07-05 15:00:13 -0400

    checking to make sure the function will work

--- mined_test.rb
+++ mined_test.rb
@@ -23,4 +23,24 @@ class Kata_test < Minitest::Test
 	def test_30_returns_what
 		assert_equal("mined minds", mm_test(30))
 	end
+	def test_10_returns_minds
+		assert_equal("minds", mm_test(10))
+	end
+	def test_18_returns_mined
+		assert_equal("mined", mm_test(18))
+	end
+	def test_33_returns_mined
+		assert_equal("mined",mm_test(33))
+	end
+	def test_60_returns_mined_minds
+		assert_equal("mined minds",mm_test(60))
+	end
+	def test_38_returns_38
+		assert_equal(38,mm_test(38))
+	end
+	def test_90_returns_mined_minds
+		assert_equal("mined minds", mm_test(90))
+	end
+
+
 end
