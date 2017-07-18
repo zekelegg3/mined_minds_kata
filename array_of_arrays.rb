a6085283c8fd87f9b083503852d5aa2a2d964201
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-07-18 13:30:02 -0400

    tried shuffle but still no assertions

--- array_of_arrays.rb
+++ array_of_arrays.rb
@@ -1,7 +1,7 @@
 def array_of_names(my_name, array_names)
 	array_names >> my_name
-		p array_names.sample(2)
-			if array_names.sample(2).length == 2
+		p array_names.shuffle(2)
+			if array_names.shuffle(2).length == 2
 				true
 			end
 end
